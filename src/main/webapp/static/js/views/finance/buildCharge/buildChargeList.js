d3bffa268876e1b82c2c4c2357d4ac1b80722795
liwb <liwb@yschome.com>
2017-12-08 16:27:25 +0800

    修复建店费用单据列表相关问题

--- src/main/webapp/static/js/views/finance/buildCharge/buildChargeList.js
+++ src/main/webapp/static/js/views/finance/buildCharge/buildChargeList.js
@@ -39,6 +39,7 @@ function initGridBranchCostList() {
     gridHandel.setGridName(gridName);
     $("#"+gridName).datagrid({
         align:'center',
+        singleSelect:true,  //单选  false多选
         rownumbers:true,    //序号
         pagination:true,    //分页
         pageSize:50,
