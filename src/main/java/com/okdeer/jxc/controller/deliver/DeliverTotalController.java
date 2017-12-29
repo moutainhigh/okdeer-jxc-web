b71794e4c30698971021b62681faab450117806a
liwb <liwb@yschome.com>
2017-12-29 18:42:07 +0800

    要货单汇总页面第二步

--- src/main/java/com/okdeer/jxc/controller/deliver/DeliverTotalController.java
+++ src/main/java/com/okdeer/jxc/controller/deliver/DeliverTotalController.java
@@ -10,11 +10,13 @@ package com.okdeer.jxc.controller.deliver;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.dubbo.config.annotation.Reference;
@@ -24,6 +26,7 @@ import com.okdeer.jxc.common.utils.PageUtils;
 import com.okdeer.jxc.common.utils.gson.GsonUtils;
 import com.okdeer.jxc.controller.BaseController;
 import com.okdeer.jxc.form.deliver.entity.DeliverForm;
+import com.okdeer.jxc.form.deliver.entity.DeliverFormList;
 import com.okdeer.jxc.form.deliver.qo.DeliverTotalFormQo;
 import com.okdeer.jxc.form.deliver.service.DeliverTotalService;
 
@@ -81,6 +84,23 @@ public class DeliverTotalController extends BaseController<DeliverTotalControlle
 
 		return "form/deliver/total/totalDataList";
 	}
+	
+	/**
+	 * @Description: 第二步子页面，查看临时数据详情列表
+	 * @param qo
+	 * @param model
+	 * @return
+	 * @author liwb
+	 * @date 2017年12月29日
+	 */
+	@RequestMapping(value = "toTotalDataItemList")
+	public String toTotalDataItemList(String formNo, Model model) {
+		LOG.debug("要货单汇总查询第二步，单据详情确认页面，单号：{}", formNo);
+		
+		model.addAttribute("formNo", formNo);
+		
+		return "form/deliver/total/totalDataItemList";
+	}
 
 	/**
 	 * @Description: 第一步，条件筛选查询数据
@@ -102,6 +122,59 @@ public class DeliverTotalController extends BaseController<DeliverTotalControlle
 			List<DeliverForm> list = deliverTotalService.getFormList(qo);
 
 			return new PageUtils<DeliverForm>(list);
+		} catch (Exception e) {
+			LOG.error("要货单汇总查询异常:", e);
+		}
+		return PageUtils.emptyPage();
+	}
+	
+	/**
+	 * @Description: 第二部，获取汇总的单据临时数据
+	 * @param qo
+	 * @return
+	 * @author liwb
+	 * @date 2017年12月29日
+	 */
+	@RequestMapping(value = "getTempDataList", method = RequestMethod.POST)
+	@ResponseBody
+	public PageUtils<DeliverForm> getTempDataList(@RequestParam("formNos[]") List<String> formNos, DeliverTotalFormQo qo) {
+		
+		LOG.debug("要货单汇总查询条件：{}", formNos);
+
+		try {
+			
+			if(CollectionUtils.isEmpty(formNos)){
+				throw new BusinessException("单号列表为空");
+			}
+			
+			qo.setFormNoList(formNos);
+			
+			String batchNo = getCurrUserId() + System.currentTimeMillis();
+			qo.setBatchNo(batchNo);
+			qo.setCurrBranchId(getCurrBranchId());
+			qo.setCurrUserId(getCurrUserId());
+			qo.setCurrUserName(getCurrentUser().getUserName());
+
+			List<DeliverForm> list = deliverTotalService.getTempDataList(qo);
+
+			return new PageUtils<DeliverForm>(list);
+		} catch (Exception e) {
+			LOG.error("要货单汇总查询异常:", e);
+		}
+		return PageUtils.emptyPage();
+	}
+	
+	@RequestMapping(value = "getTempDataItemList", method = RequestMethod.POST)
+	@ResponseBody
+	public PageUtils<DeliverFormList> getTempDataItemList(String formNo) {
+		
+		LOG.debug("详情单号：{}", formNo);
+		
+		try {
+			
+			List<DeliverFormList> list = deliverTotalService.getTempDetailList(formNo);
+			
+			return new PageUtils<DeliverFormList>(list);
 		} catch (Exception e) {
 			LOG.error("要货单汇总查询异常:", e);
 		}
