098e95223ded8a1d31fd99e3909514b0743c1062
yangyq02 <yangyq02@yschome.com>
2018-01-08 15:16:07 +0800

    修改商品出入库明细报表

--- src/main/java/com/okdeer/jxc/controller/report/GoodsOutInDetailReportController.java
+++ src/main/java/com/okdeer/jxc/controller/report/GoodsOutInDetailReportController.java
@@ -3,7 +3,7 @@
  *@Author: liux01
  *@Date: 2016年10月26日 
  *@Copyright: ©2014-2020 www.yschome.com Inc. All rights reserved. 
- */    
+ */
 package com.okdeer.jxc.controller.report;
 
 import java.util.List;
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.okdeer.jxc.common.constant.GpeMarkContrant;
 import com.okdeer.jxc.common.controller.AbstractSimpleGpeController;
+import com.okdeer.jxc.report.qo.GoodsOutInDetailQo;
 import com.okdeer.jxc.report.service.GoodsOutInDetailServiceApi;
 import com.okdeer.jxc.report.vo.GoodsOutInDetailVo;
 import com.okdeer.retail.common.page.EasyUIPageInfo;
@@ -35,14 +36,15 @@ import com.okdeer.retail.framework.gpe.bean.CustomMarkBean;
  */
 @Controller
 @RequestMapping("goods/goodsDetail")
-public class GoodsOutInDetailReportController extends AbstractSimpleGpeController<GoodsOutInDetailVo, GoodsOutInDetailVo>{
-	
+public class GoodsOutInDetailReportController extends
+		AbstractSimpleGpeController<GoodsOutInDetailQo, GoodsOutInDetailVo> {
+
 	/**
 	 * 商品出入库明细报表Dubbo接口
 	 */
 	@Reference(version = "1.0.0", check = false)
 	private GoodsOutInDetailServiceApi goodsOutInDetailServiceApi;
-	
+
 	/**
 	 * 
 	 * @Description: 报表页跳转
@@ -51,15 +53,15 @@ public class GoodsOutInDetailReportController extends AbstractSimpleGpeControlle
 	 * @date 2017年5月5日
 	 */
 	@RequestMapping(value = "/list")
-	public String list(){
+	public String list() {
 		return "/report/goods/goodsOutInDetailReport";
 	}
-	
 
 	@Override
 	protected CustomMarkBean getCustomMark() {
-		return new CustomMarkBean(GpeMarkContrant.MOUDLE_STOCK, GpeMarkContrant.SECTION_STOCK_DETAIL, GpeMarkContrant.KEY_LIST);
-		
+		return new CustomMarkBean(GpeMarkContrant.MOUDLE_STOCK, GpeMarkContrant.SECTION_STOCK_DETAIL,
+				GpeMarkContrant.KEY_LIST);
+
 	}
 
 	@Override
@@ -79,27 +81,27 @@ public class GoodsOutInDetailReportController extends AbstractSimpleGpeControlle
 	}
 
 	@Override
-	protected EasyUIPageInfo<GoodsOutInDetailVo> queryListPage(GoodsOutInDetailVo qo) {
-		if(StringUtils.isEmpty(qo.getBranchCompleCode())){
+	protected EasyUIPageInfo<GoodsOutInDetailVo> queryListPage(GoodsOutInDetailQo qo) {
+		if (StringUtils.isEmpty(qo.getBranchCompleCode())) {
 			qo.setBranchCompleCode(this.getCurrBranchCompleCode());
 		}
-		EasyUIPageInfo<GoodsOutInDetailVo> page= goodsOutInDetailServiceApi.getGoodsOutInDetailList(qo);
+		EasyUIPageInfo<GoodsOutInDetailVo> page = goodsOutInDetailServiceApi.getGoodsOutInDetailList(qo);
 		return page;
 	}
 
 	@Override
-	protected GoodsOutInDetailVo queryTotal(GoodsOutInDetailVo qo) {
-		if(StringUtils.isEmpty(qo.getBranchCompleCode())){
+	protected GoodsOutInDetailVo queryTotal(GoodsOutInDetailQo qo) {
+		if (StringUtils.isEmpty(qo.getBranchCompleCode())) {
 			qo.setBranchCompleCode(this.getCurrBranchCompleCode());
 		}
-		GoodsOutInDetailVo vo= goodsOutInDetailServiceApi.queryGoodsOutInDetailCountSum(qo);
+		GoodsOutInDetailVo vo = goodsOutInDetailServiceApi.queryGoodsOutInDetailCountSum(qo);
 		vo.setBranchName("合计:");
 		return vo;
 	}
 
 	@Override
-	protected List<GoodsOutInDetailVo> queryList(GoodsOutInDetailVo qo) {
-		if(StringUtils.isEmpty(qo.getBranchCompleCode())){
+	protected List<GoodsOutInDetailVo> queryList(GoodsOutInDetailQo qo) {
+		if (StringUtils.isEmpty(qo.getBranchCompleCode())) {
 			qo.setBranchCompleCode(this.getCurrBranchCompleCode());
 		}
 		return goodsOutInDetailServiceApi.exportList(qo);
